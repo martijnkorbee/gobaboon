cbe748e595a6d1f915bce085e4922d6ebb06f8f8
Martijn Korbee <48735177+martijnkorbee@users.noreply.github.com>
2023-01-16 15:53:56 +0100

    Update baboon.go

--- baboon.go
+++ baboon.go
@@ -67,13 +67,17 @@ func (b *Baboon) Init(c Config) error {
 
 	// check if not exist create expected folders for a new baboon app
 	fnames := []string{
+		"cmd/web/app"
+		"cmd/web/bin"
 		"http/handlers",
 		"http/middleware",
+		"http/routes",
 		"database/models",
 		"database/migrations",
 		"db-data",
 		"templates/views",
 		"templates/mail",
+		"public/static",
 		"logs",
 		"tmp",
 	}
