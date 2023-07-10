e413927701b31f4fa52ee690693b99b835432125
martijnkorbee <korbee@pm.me>
2023-07-10 18:41:30 +0200

    fix make session

--- tools/baboonctl/cmd/make_session.go
+++ tools/baboonctl/cmd/make_session.go
@@ -3,7 +3,6 @@ package cmd
 import (
 	"fmt"
 	ctl "github.com/martijnkorbee/gobaboon/tools/baboonctl/internal/util"
-	"os"
 	"time"
 
 	"github.com/spf13/cobra"
@@ -12,33 +11,36 @@ import (
 var makeSessionCmd = &cobra.Command{
 	Use:   "session",
 	Short: "Make table migrations for baboon sessions",
-	Long: `Creates and runs up and down migrations for the baboon server's persistent sessions.
-Should be called from the root directory of a baboon app that has a valid .env file.
+	Long: `Creates up and down migrations for the server's persistent sessions.
 
-NOTE: supported databases postgres, mysql/mariadb, sqlite
+SUPPORTED DATABASES: [postgres, mysql, mariadb, sqlite]
 `,
 	Run: func(cmd *cobra.Command, args []string) {
 		var (
-			dbType        = os.Getenv("DATABASE_TYPE")
 			migrationName = fmt.Sprintf("%d_create_sessions_table", time.Now().UnixMicro())
-			upFilePath    = rootPath + "/database/migrations/" + migrationName + ".up.sql"
-			downFilePath  = rootPath + "/database/migrations/" + migrationName + ".down.sql"
+			upSource      = "templates/migrations/sessions." + dbType + ".up.sql"
+			downSource    = "templates/migrations/sessions." + dbType + ".down.sql"
+			upTarget      = rootPath + "/internal/database/migrations/" + migrationName + ".up.sql"
+			downTarget    = rootPath + "/internal/database/migrations/" + migrationName + ".down.sql"
 		)
 
 		// create up file
-		err := ctl.CopyFileFromTemplate(templateFS, "templates/migrations/sessions."+dbType+".up.sql", upFilePath)
+		err := ctl.CopyFileFromTemplate(templateFS, upSource, upTarget)
 		if err != nil {
 			ctl.PrintFatal("failed to create up file", err)
 		}
 
 		// create down file
-		err = ctl.CopyFileFromTemplate(templateFS, "templates/migrations/sessions."+dbType+".down.sql", downFilePath)
+		err = ctl.CopyFileFromTemplate(templateFS, downSource, downTarget)
 		if err != nil {
 			ctl.PrintFatal("failed to create down file", err)
 		}
 
-		ctl.PrintInfo("created migrations, calling migrate up ...")
-
-		migrateUp()
+		ctl.PrintResult("created migrations for", "persistent sessions")
 	},
+}
+
+func init() {
+	makeSessionCmd.Flags().StringVarP(&dbType, "db-type", "t", "", "specify your database type")
+	makeSessionCmd.MarkFlagRequired("db-type")
 }
