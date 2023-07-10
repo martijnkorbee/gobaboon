06e01b3f6f06045b8274eb3f681fe8b791ccd419
martijnkorbee <korbee@pm.me>
2023-07-10 16:56:06 +0200

    authentication

--- internal/http/routes/routes-api.go
+++ internal/http/routes/routes-api.go
@@ -8,24 +8,10 @@ func (ar *AppRoutes) RoutesAPI() *chi.Mux {
 	// API routes
 	r := chi.NewRouter()
 
-	// add your middleware here
-
 	// used to check health
 	r.Get("/ping", ar.Handlers.Ping) // default route
 
-	// users
-	r.Route("/users/{email}", func(r chi.Router) {
-		r.MethodFunc("POST", "/", ar.Handlers.UsersAdd)
-		r.MethodFunc("DELETE", "/", ar.Handlers.UsersDelete)
-	})
-
-	r.Route("/v1", func(r chi.Router) {
-		// these routes are protected
-		r.Use(ar.Middleware.AuthToken)
-
-		// used to check health
-		r.Get("/ping", ar.Handlers.Ping) // default route
-	})
+	// add your middleware and routes here
 
 	return r
 }
